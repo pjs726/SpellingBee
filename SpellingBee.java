e35108c360a463c682d3a15b6cc01b337e6f35af
Paramjot Singh <pjssunlight@gmail.com>
2023-08-18 12:45:36 -0700

    Completed Milestone 1

--- SpellingBee.java
+++ SpellingBee.java
@@ -7,6 +7,7 @@
 
 import edu.willamette.cs1.spellingbee.SpellingBeeGraphics;
 import java.awt.Color;
+import java.util.*;
 
 public class SpellingBee {
 
@@ -17,7 +18,38 @@ public class SpellingBee {
     }
 
     private void puzzleAction(String s) {
-        sbg.showMessage("puzzleAction is not yet implemented", Color.RED);
+        
+        s = s.toUpperCase();
+
+        /* Check to see if string length is 7 */
+        if (s.length() != 7)
+        {
+            sbg.showMessage("Puzzle must have seven letters.", Color.RED);
+            return;
+        }
+
+        HashSet<Character> charSet = new HashSet<Character>();
+        String temp = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            /* Check to see if input character is a letter. */
+            if (!Character.isLetter(s.charAt(i))) {
+                sbg.showMessage("All input characters must be letters.", Color.RED);
+                return;
+            }
+
+            /* Check to see if input character is a duplicate. */
+            if (charSet.contains(s.charAt(i))) {
+                sbg.showMessage("All input characters must be unique.", Color.RED);
+                return; 
+            }
+            charSet.add(s.charAt(i));
+
+            
+            temp += s.charAt(i);
+        }
+        sbg.showMessage("");
+        sbg.setBeehiveLetters(temp);
     }
 
     private void solveAction() {
